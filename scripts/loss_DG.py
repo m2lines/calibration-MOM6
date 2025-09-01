5f7a2c2ff81c389fafbb9b4419cf517203cba274
Pavel Perezhogin <pperezhogin@gmail.com>
2025-09-01 12:49:15 -0400

    Loss function for variability

---
+++ scripts/loss_DG.py
@@ -0,0 +1,34 @@
+import xarray as xr
+import numpy as np
+import sys
+sys.path.append('../src-double-gyre')
+from helpers.computational_tools import compute_isotropic_KE
+
+def variability_metrics(_e, _u, _v, static, Time=slice(1825,3650), coarse_factor=2):
+    '''
+    This function computes metrics of interest which we would like to optimize:
+    * STD of interfaces
+    * Isotropic EKE spectrum
+
+    Note: we could compute EKE spectrum from interface itself,
+    however, this does not translate to the subsurface layer
+    
+    Note: we use isotopic metrics to do not give directional information
+    to the optimized ANN as it may overfit to predict zonal flows
+    '''
+    e = _e.sel(Time=Time)
+    u = _u.sel(Time=Time)
+    v = _v.sel(Time=Time)
+
+    e_std = e.std('Time').coarsen({'xh':coarse_factor, 'yh':coarse_factor}).mean().compute()
+
+    u_prime = u - u.mean('Time')
+    v_prime = v - v.mean('Time')
+
+    EKE_spectrum = compute_isotropic_KE(u_prime, v_prime, static.dxT, static.dyT).mean('Time').compute()
+
+    ds = xr.Dataset()
+    ds['e_std'] = e_std
+    ds['EKE_spectrum'] = EKE_spectrum
+
+    return ds
