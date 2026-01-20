Fix RNG in Julia and Gamma scaling factor for complete reproducibility of old experiement

calibration_driver/driver.py
@@ -45,10 +45,10 @@ observation_vector = np.concatenate(observation_vector)
 gamma_vector = []
 for key in config["eki"]["gamma_vector"]:
     gamma_vector.append(observation_netcdf[key].values.ravel())
-gamma_vector = np.concatenate(gamma_vector)
+gamma_vector = config["eki"]["gamma_scaling_factor"] * np.concatenate(gamma_vector)
 
 ############ Initialize EKI process #############
-initialize_eki(observation_vector, gamma_vector, initial_ensemble, config["eki"]["scheduler"], config["eki"]["inversion"], config["eki"]["seed"], optimization_folder_pwd)
+initialize_eki(observation_vector, gamma_vector, initial_ensemble, config["eki"]["scheduler"], config["eki"]["inversion"], config["eki"]["seed_julia"], optimization_folder_pwd)
 
 for iteration in range(args.latest_iteration, config["eki"]["n_iterations"]):
     print(f'################ iteration {iteration} ####################')