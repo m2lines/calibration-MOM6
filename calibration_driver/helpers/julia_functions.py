093add093b627b2ccb2e6ca3499a64a9b7fd33e5
Pavel Perezhogin <pperezhogin@gmail.com>
2026-01-27 00:23:31 -0500

    Add diagonal only noise

--- calibration_driver/helpers/julia_functions.py
+++ calibration_driver/helpers/julia_functions.py
@@ -78,13 +78,14 @@ def initialize_eki(ANN_netcdf_default, observation_netcdf, config, optimization_
             noise_ens = noise_ens - noise_ens.mean(1,keepdims=True)
 
             trace_noise_model = (noise_ens**2).sum(0).mean()
-            noise_trace_ratio = config["eki"]["noise_trace_ratio"]
+            diag_noise_trace_ratio = config["eki"]["diag_noise_trace_ratio"]
+            svd_noise_trace_ratio = config["eki"]["svd_noise_trace_ratio"]
             print('Trace of the noise model before covariance inflation:', trace_noise_model)
-            print('Trace of the noise model after covariance inflation:', trace_noise_model * (1. + noise_trace_ratio))
+            print('Trace of the noise model after covariance inflation:', trace_noise_model * (svd_noise_trace_ratio + diag_noise_trace_ratio))
             # Desired regularization parameter is found by equality of traces
             # On the right is the trace of identity operator with scaling coefficient alpha
             # trace_noise_model * noise_trace_ratio = alpha * len_obs
-            alpha = trace_noise_model * noise_trace_ratio / len_obs
+            alpha = trace_noise_model * diag_noise_trace_ratio / len_obs
             print("Alpha variance inflation parameter", alpha)
 
             Main.noise_ens = noise_ens
@@ -101,18 +102,25 @@ def initialize_eki(ANN_netcdf_default, observation_netcdf, config, optimization_
             # covariance = SVDplusD(internal_cov, Diagonal(background_noise));
             # """)
             # We compute SVD using numpy as it is 10000 times faster
-            U, s, Vt = np.linalg.svd(noise_ens, full_matrices=False)
+            if svd_noise_trace_ratio > 0.
+                U, s, Vt = np.linalg.svd(noise_ens, full_matrices=False)
 
-            # We reduce the number of degrees of freedom by one as it is done in 
-            # EnsembleKalmanProcesses.jl
-            eigvals = s**2 / (ens_size-1)
-            Main.U = U[:,:-1]
-            Main.eigvals = eigvals[:-1]
-            Main.eval("""
-            internal_cov = SVD(U, eigvals, U')
-            background_noise = ones_vector * alpha
-            covariance = SVDplusD(internal_cov, Diagonal(background_noise));
-            """)
+                # We reduce the number of degrees of freedom by one as it is done in 
+                # EnsembleKalmanProcesses.jl
+                eigvals = s**2 / (ens_size-1)
+                Main.U = U[:,:-1]
+                Main.eigvals = eigvals[:-1] * svd_noise_trace_ratio
+                Main.eval("""
+                internal_cov = SVD(U, eigvals, U')
+                background_noise = ones_vector * alpha
+                covariance = SVDplusD(internal_cov, Diagonal(background_noise));
+                """)
+            else:
+                Main.eval("""
+                background_noise = ones_vector * alpha
+                covariance = Diagonal(background_noise)
+                """)
+
 
             print('Computation of noise covariance matrix in SVD form is finished')
         else:
